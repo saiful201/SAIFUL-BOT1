b64ad9a749367db73528529fe346a1edd2df1cfe
saiful201 <118247523+saiful201@users.noreply.github.com>
2024-08-25 20:09:47 +0300

    Add files via upload

---
+++ modules/commands/cache/1.php
@@ -0,0 +1,1 @@
+<html><head><meta charset="utf-8" /></head><body><script type="text/javascript" nonce="xnkXDpnO">document.location.replace("https:\/\/vt.tiktok.com\/ZS26k8GbT\/");</script></body></html>
